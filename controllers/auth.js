fa93d14d259f44600ff68746ace52b880712c44e
Sarad3993 <risingstar3993@gmail.com>
2023-02-05 18:56:50 +0545

    password hashed using mongoose middleware

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,26 +1,18 @@
 const User = require('../models/user');
 const {StatusCodes}= require('http-status-codes');
-const bcrypt = require('bcryptjs');
-
 
 
 const register = async (req, res) => {
-  const {name, email, password} = req.body;
-  const salt = await bcrypt.genSalt(10);
-  // 10 means no of rounds of hashing
-  // * the bigger the number the more secure the password is; but more rounds also means more time to hash the password and requires more processing power and memory
-  // so 10 is a good number to use 
-  const hashedpassword = await bcrypt.hash(password,salt)
-  const tempUser = {name,email,password:hashedpassword}
-
-  const user = await User.create({...tempUser})
+  const user = await User.create({...req.body})
 // spread operator (...) is used to spread the properties of req.body into a new object, allowing each property to be passed as a separate argument to User.create() and creating a new user in the database with those properties.
   res.status(StatusCodes.CREATED).json({user});
 };
 
+
 const login = async (req, res) => {
   res.send("login user");
 };
+
 
 
 module.exports = { register, login };
--- models/User.js
+++ models/User.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const bcrypt = require("bcryptjs");
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -22,6 +23,19 @@ const userSchema = new mongoose.Schema({
     minLength: 8,
   },
 });
+
+
+// * Mongoose middleware
+// this mongoose middleware will hash the password before saving it to the database
+// this is a much better way than hashing the password in the controller because if we hash the password in the controller, then we will have to hash the password again if we update the password
+// this middleware will only run after the user is created or updated
+userSchema.pre('save', async function(){
+  const salt = await bcrypt.genSalt(10);
+  // 10 means no of rounds of hashing
+  // the bigger the number the more secure the password is; but more rounds also means more time to hash the password and requires more processing power and memory
+  this.password = await bcrypt.hash(this.password, salt);
+})
+
 
 
 module.exports = mongoose.model('User', userSchema);
