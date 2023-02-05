e9d918edbec2a41640aa195ad12e4871713a01f5
Sarad3993 <risingstar3993@gmail.com>
2023-02-05 21:16:12 +0545

    register and login user complete

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,20 +1,46 @@
 const User = require("../models/user");
 const { StatusCodes } = require("http-status-codes");
+const { BadRequestError, UnauthenticatedError } = require("../errors");
 
 const register = async (req, res) => {
   const user = await User.create({ ...req.body });
   // spread operator (...) is used to spread the properties of req.body into a new object, allowing each property to be passed as a separate argument to User.create() and creating a new user in the database with those properties.
 
-  const token = user.createJWT(); 
+  const token = user.createJWT();
   // createJWT() is a method defined in the User model. here we just invoked it for the user we just created.
 
   res.status(StatusCodes.CREATED).json({ user: { name: user.name }, token });
 };
 
 
+
 const login = async (req, res) => {
-  res.send("login user");
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    throw new BadRequestError("Please provide email and password");
+  }
+
+  const user = await User.findOne({ email });
+
+  if (!user) {
+    throw new UnauthenticatedError("Invalid Credentials!");
+  }
+
+  // compare password
+  const isPassswordMatch = await user.comparePassword(password);
+  // invoke comparePassword() method defined in the User model and pass the password provided by the user in the request as a parameter to that method of User model
+
+  if (!isPassswordMatch) {
+    throw new UnauthenticatedError("Invalid Credentials!");
+  }
+
+  // sign jwt if everything is ok ie if the user is found and the password is correct
+  const token = user.createJWT();
+
+  res.status(StatusCodes.OK).json({ user: { name: user.name }, token });
 };
+
 
 
 module.exports = { register, login };
--- models/User.js
+++ models/User.js
@@ -1,4 +1,4 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 
@@ -30,19 +30,32 @@ const userSchema = new mongoose.Schema({
 // this mongoose middleware will hash the password before saving it to the database
 // this is a much better way than hashing the password in the controller because if we hash the password in the controller, then we will have to hash the password again if we update the password
 // this middleware will only run after the user is created or updated
-userSchema.pre('save', async function(){
+userSchema.pre("save", async function () {
   const salt = await bcrypt.genSalt(10);
   // 10 means no of rounds of hashing
   // the bigger the number the more secure the password is; but more rounds also means more time to hash the password and requires more processing power and memory
   this.password = await bcrypt.hash(this.password, salt);
-})
+});
 
 
-// * Mongoose methods
+// * Mongoose methods (instance methods on the schema)
 // we signed the jwt using mongose methods instead of signing in the controller cuz if we sign the jwt inside controller, then we will have to sign the jwt again if we update the user
-// but if we sign the jwt in the mongoose methods, then we can just call this method to sign the jwt anywhere in the application 
-userSchema.methods.createJWT = function(){
-    return jwt.sign({userId:this._id,name:this.name}, process.env.JWT_SECRET, {expiresIn:'30d',})
+// but if we sign the jwt in the mongoose methods, then we can just call this method to sign the jwt anywhere in the application
+userSchema.methods.createJWT = function () {
+  return jwt.sign(
+    { userId: this._id, name: this.name },
+    process.env.JWT_SECRET,
+    { expiresIn: process.env.JWT_EXPIRES_IN }
+  );
+};
+
+
+// for comparing the password in the database with the password provided by the user
+// compare method of bcrypt is used to compare the password provided by the user in the request with the password in the database
+userSchema.methods.comparePassword = async function (password){
+    const isMatch = await bcrypt.compare(password, this.password);
+    return isMatch;
 }
 
-module.exports = mongoose.model('User', userSchema);
+
+module.exports = mongoose.model("User", userSchema);
