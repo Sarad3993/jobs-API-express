8010b08389e7bed204072598fa8d78b6239e70b0
Sarad3993 <risingstar3993@gmail.com>
2023-02-08 20:17:35 +0545

    few changes

--- routes/jobs.js
+++ routes/jobs.js
@@ -3,9 +3,14 @@ const {getAllJobs,getJob,createJob,updateJob,deleteJob} = require("../controller
 
 
 
+
 router.route("/").get(getAllJobs).post(createJob);
 
 router.route("/:id").get(getJob).patch(updateJob).delete(deleteJob);
+// here patch is used instead of put because we are not updating the whole job. We are just updating the company and position fields of the job.
+// but if we were updating the whole job then we would have used put. Whole job means all the fields of the job.
+// put --> updates the whole job
+// patch --> updates only the fields sent by the user in the request body. 
 
 
 module.exports = router;
